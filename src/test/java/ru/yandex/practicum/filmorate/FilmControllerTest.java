9cb22e0a7c114736238de3601a0ccba381d93e0b
Ян Борис Сергеевич <mr.boris.yan@yandex.ru>
2023-08-31 20:29:12 +0300

    HW10R1 - add simple test

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+public class FilmControllerTest {
+    private final FilmStorage storage = new InMemoryFilmStorage();
+    private final UserStorage userStorage = new InMemoryUserStorage();
+    private final UserService userService = new UserService(userStorage);
+    private final FilmService service = new FilmService(storage, userService);
+    private final FilmController controller = new FilmController(storage, service);
+
+    private final Film film = Film.builder()
+            .id(1L)
+            .name("Фильм на тест")
+            .description("Описание")
+            .releaseDate(LocalDate.of(2000, 12, 28))
+            .duration(120)
+            .likes(new HashSet<>())
+            .build();
+
+    @Test
+    void addFilmTest() {
+        controller.addFilm(film);
+
+        Assertions.assertEquals(1, controller.getFilms().size());
+    }
+
+}
